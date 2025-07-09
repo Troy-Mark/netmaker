ab1f763efdaf7b7dde942f345da6514e7019bb65
Troy Mark <155806998+Troy-Mark@users.noreply.github.com>
2025-07-09 10:39:53 +0800

    Update nm-quick.sh

--- scripts/nm-quick.sh
+++ scripts/nm-quick.sh
@@ -380,7 +380,7 @@ install_dependencies() {
 		curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 		chmod a+r /etc/apt/keyrings/docker.asc
 		echo \
-		  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://dmirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+		  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
 		  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 		  tee /etc/apt/sources.list.d/docker.list > /dev/null
 		apt update
